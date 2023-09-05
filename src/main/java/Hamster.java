9754bf1f29a7ee26f8342cedf7d688deef5c033a
Constantine Kim <electro.passion92@gmail.com>
2023-09-06 00:04:46 +0900

    Реализация equals/hashCode

--- src/main/java/Hamster.java
+++ src/main/java/Hamster.java
@@ -4,4 +4,21 @@ public class Hamster {
     public Hamster(String name) {
         this.name = name;
     }
+
+    // Функция boolean equals(Object object) определяет, равен ли один объект другому.
+    // Функция int hashCode() возвращает хеш-код, связанный с вызывающим объектом.
+    // Эти функции нужны для корректной работы HashSet, подробнее о них поговорим в одной из следующих тем спринта
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Hamster hamster = (Hamster) o;
+        return name.equals(hamster.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
